3eb39e5e05f5665f3787e8e3a12b7f309302e383
DiegoCuba <diego.netsolutions@gmail.com>
2021-01-03 06:06:12 +0000

    Added Procfile

--- routes/api.php
+++ routes/api.php
@@ -35,3 +35,6 @@ Route::middleware('auth:sanctum')->post('/logoutALL',function(Request $request){
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });                
+Route::get('/test', function (Request $request) {
+    return 'TEST SUCCESS!!!';
+});  
